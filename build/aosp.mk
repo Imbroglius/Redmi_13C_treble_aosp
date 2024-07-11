4054e10d9cecb0e04daae4cd8a7413ee912d32ba
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-11 15:15:39 +0200

    Update aosp.mk

--- build/aosp.mk
+++ build/aosp.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/ponces/config/common.mk)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/Imbroglius/imbroglios_gsi/android-14.0/config/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/Imbroglius/imbroglios_gsi/android-15.0/config/ota.json
