eb411fdd1a553c50e2c36cd2505aeae11e8e8228
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-08-10 19:36:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ syncRepos() {
 
 clonePriv() {
     echo "Import signing keys"
-    git clone https://github.com/imbroglius/vendor_daniel-priv vendor/daniel-priv
+    git clone https://imbroglius@github.com/imbroglius/vendor_daniel-priv vendor/daniel-priv
 }
 
 
