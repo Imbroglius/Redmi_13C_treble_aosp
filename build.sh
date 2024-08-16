9b0497a5a6bd6e6785aa6e89aa88042fcffb46b9
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-08-16 18:13:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,8 +21,8 @@ initRepos() {
 
     echo "--> Preparing local manifest"
     mkdir -p .repo/local_manifests
-    cp $BL/build/default.xml .repo/local_manifests/default.xml
-    cp $BL/build/remove.xml .repo/local_manifests/remove.xml
+    cp /$BL/build/default.xml .repo/local_manifests/default.xml
+    cp /$BL/build/remove.xml .repo/local_manifests/remove.xml
     echo
 }
 
